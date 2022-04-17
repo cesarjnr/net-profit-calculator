7aaae2ae5b86a34e160afc5c0ca4669aade03b71
César Júnior <cesar.jnr@hotmail.com>
2022-04-17 20:23:15 -0300

    Change styles in sidebar component and earnings page

--- components/sidebar/index.tsx
+++ components/sidebar/index.tsx
@@ -6,25 +6,25 @@ interface MenuItem {
 }
 
 export default function Sidebar() {
-    const menuItems: MenuItem[] = [
-        { page: 'Dashboard', link: '/' },
-        { page: 'Earnings', link: '/earnings'},
-        { page: 'Compensations', link: '/compensations' },
-        { page: 'Calculator', link: '/calculator' }
-    ];
+  const menuItems: MenuItem[] = [
+    { page: 'Dashboard', link: '/' },
+    { page: 'Earnings', link: '/earnings'},
+    { page: 'Compensations', link: '/compensations' },
+    { page: 'Calculator', link: '/calculator' }
+  ];
 
-    return (
-        <nav className="flex flex-col bg-primary">
-            <div className="grow text-2xl flex justify-center items-center text-secondary font-bold">IC</div>
-            <ul className="grow-[5] list-none flex flex-col text-secondary text-sm">
-                {menuItems.map((item) => (
-                    <li key={item.link} className="px-8 py-2 cursor-pointer hover:bg-secondary-transparent">
-                        <Link href={item.link}>
-                            {item.page}
-                        </Link>
-                    </li>
-                ))}
-            </ul>
-        </nav>
-    );
+  return (
+    <nav className="flex flex-col bg-primary-800">
+      <div className="grow text-2xl flex justify-center items-center text-secondary-400 font-bold">IC</div>
+      <ul className="grow-[5] list-none flex flex-col text-secondary-400 text-sm">
+        {menuItems.map((item) => (
+          <li key={item.link} className="px-8 py-2 cursor-pointer hover:bg-secondary-400/10">
+            <Link href={item.link}>
+              {item.page}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 }
--- pages/earnings/index.tsx
+++ pages/earnings/index.tsx
@@ -1,32 +1,78 @@
+import { Fragment, useState } from 'react';
+
+import Modal from '../../components/modal';
+import Button, { ButtonVariant, ButtonType } from '../../components/button';
+import Input, { InputType } from '../../components/input';
+
 export default function Earning() {
-    return (
-        <div className="h-full flex justify-center items-center">
-            <table className="table-auto border border-primary w-4/5">
-                <thead>
-                    <tr className="bg-primary text-secondary">
-                        <th className="border border-primary p-2">Date</th>
-                        <th className="border border-primary p-2">Earning</th>
-                    </tr>
-                </thead>
-                <tbody className="text-primary">
-                    <tr>
-                        <td className="border border-primary p-2">01/2022</td>
-                        <td className="border border-primary p-2">R$12.000,00</td>
-                    </tr>
-                    <tr>
-                        <td className="border border-primary p-2">02/2022</td>
-                        <td className="border border-primary p-2">R$12.000,00</td>
-                    </tr>
-                    <tr>
-                        <td className="border border-primary p-2">03/2022</td>
-                        <td className="border border-primary p-2">R$18.000,00</td>
-                    </tr>
-                    <tr>
-                        <td className="border border-primary p-2">04/2022</td>
-                        <td className="border border-primary p-2">R$8.000,00</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    );
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <Fragment>
+      <div className="mb-5 flex justify-end">
+        <Button
+          variant={ButtonVariant.Contained}
+          type={ButtonType.Button}
+          text='New Earning'
+          onClick={() => setShowModal(true) }
+        />
+      </div>
+
+      <table className="table-auto border border-primary-800 w-full">
+        <thead>
+          <tr className="bg-primary-800 text-secondary-400 text-sm">
+            <th className="border border-primary-800 p-3 font-medium">Date</th>
+            <th className="border border-primary-800 p-3 font-medium">Earning</th>
+          </tr>
+        </thead>
+        <tbody className="text-primary-800 text-sm">
+          <tr>
+            <td className="border border-primary-100 p-2">01/2022</td>
+            <td className="border border-primary-100 p-2">R$12.000,00</td>
+          </tr>
+          <tr>
+            <td className="border border-primary-100 p-2">02/2022</td>
+            <td className="border border-primary-100 p-2">R$12.000,00</td>
+          </tr>
+          <tr>
+            <td className="border border-primary-100 p-2">03/2022</td>
+            <td className="border border-primary-100 p-2">R$18.000,00</td>
+          </tr>
+          <tr>
+            <td className="border border-primary-100 p-2">04/2022</td>
+            <td className="border border-primary-100 p-2">R$8.000,00</td>
+          </tr>
+        </tbody>
+        <tfoot>
+          <tr className="bg-primary-50 text-primary-800 text-sm font-medium">
+            <td className="border border-primary-100 p-2">Total</td>
+            <td className="border border-primary-100 p-2">R$50.000,00</td>
+          </tr>
+        </tfoot>
+      </table>
+
+      <Modal open={showModal} title="New Earning" onClose={() => setShowModal(false)}>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <Input name='date' label='Date' type={InputType.Date} />
+          <Input name='value' label='Value (R$)' type={InputType.Text} />
+
+          <div className="flex gap-5">
+            <Button
+              variant={ButtonVariant.Contained}
+              type={ButtonType.Submit}
+              text='Create'
+              fullWidth
+            />
+            <Button
+              variant={ButtonVariant.Outlined}
+              type={ButtonType.Button}
+              text='Cancel'
+              onClick={() => setShowModal(false)}
+              fullWidth
+            />
+          </div>
+        </form>
+      </Modal>
+    </Fragment>
+  );
 }
