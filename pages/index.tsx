a9d6020dcb418693d890f73f948b150443e86ea4
César Júnior <cesar.jnr@hotmail.com>
2022-05-15 02:37:12 -0300

    Refactor the dashboard page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,30 @@
 import useDashboard from '../hooks/useDashboard';
 
+interface Card {
+  title: string;
+  content: string;
+}
+
 export default function Home() {
   const {
     calculateLastTwelveMonthsEarnings,
     calculateLastTwelveMonthsCompensations,
     calculateNextMonthCompensation
   } = useDashboard();
+  const cards: Card[] = [
+    { title: 'Last 12 months earnings', content: calculateLastTwelveMonthsEarnings() },
+    { title: 'Last 12 months compensations', content: calculateLastTwelveMonthsCompensations() },
+    { title: 'Next Month\'s Compensation', content: calculateNextMonthCompensation() }
+  ];
 
   return (
     <div className="h-full flex justify-center items-center gap-10">
-      <div className="rounded-lg shadow-md p-6 flex flex-col items-center gap-3">
-        <h1 className="text-primary-800 text-2xl font-bold">Last 12 months earnings</h1>
-        <span className="font-bold">{calculateLastTwelveMonthsEarnings()}</span>
-      </div>
-
-      <div className="rounded-lg shadow-md p-6 flex flex-col items-center gap-3">
-        <h1 className="text-primary-800 text-2xl font-bold">Last 12 months compensations</h1>
-        <span className="font-bold">{calculateLastTwelveMonthsCompensations()}</span>
-      </div>
-
-      <div className="rounded-lg shadow-md p-6 flex flex-col items-center gap-3">
-        <h1 className="text-primary-800 text-2xl font-bold">Next Month's Compensation</h1>
-        <span className="font-bold">{calculateNextMonthCompensation()}</span>
-      </div>
+      {cards.map((card) => (
+        <div className="bg-primary-800 rounded-lg shadow-md p-6 flex flex-col items-center gap-3">
+          <h1 className="text-secondary-400 text-2xl font-bold">{card.title}</h1>
+          <span className="text-white font-bold">{card.content}</span>
+        </div>
+      ))}
     </div>
   );
 }
